2a2786ff32d5f9d907a1f7bd60be816cd24e52ab
ffaassaa <107076427+ffaassaa@users.noreply.github.com>
2023-11-21 23:54:19 -0800

    Update HelloCS151.java
    added extra circles because they are pretty cool

--- OliverZeyenJavaFX/src/application/HelloCS151.java
+++ OliverZeyenJavaFX/src/application/HelloCS151.java
@@ -3,9 +3,13 @@ package application;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 /*
  * IMPORTANT: This one is just silly for fun I am having a good day
@@ -58,6 +62,15 @@ public class HelloCS151 extends Application {
 		root.getChildren().add(hi);
 		root.getChildren().add(notHi);
 		primaryStage.setScene(new Scene(root, 500, 250));
+		
+		Group circles = new Group();
+		for (int i = 0; i < 15; i++) {
+			Circle c = new Circle(250, Color.web("blue", 0.05));
+			c.setStrokeType(StrokeType.OUTSIDE);
+			c.setStroke(Color.web("blue", 0.05));
+			c.setStrokeWidth(5);
+			circles.getChildren().add(c);
+		}
 		primaryStage.show();
 	}
 }
