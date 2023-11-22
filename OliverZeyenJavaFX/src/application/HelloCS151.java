4de888b12bc404f66800df089c520b1e6bd42974
ffaassaa <107076427+ffaassaa@users.noreply.github.com>
2023-11-21 23:39:22 -0800

    Adding simple JavaFX project

---
+++ OliverZeyenJavaFX/src/application/HelloCS151.java
@@ -0,0 +1,63 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+/*
+ * IMPORTANT: This one is just silly for fun I am having a good day
+ */
+public class HelloCS151 extends Application {
+	public static int meanCounter;
+	
+	public static void main(String[] args) {
+		HelloCS151.meanCounter = 0;
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Hello CS 151!");
+		Button hi = new Button();
+		Button notHi = new Button();
+		hi.setText("Say 'Hello CS 151!'");
+		hi.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Thanks for saying hello CS 151!");
+			}
+		});
+	
+		notHi.setText("Don't say 'Hello CS 151' (Don't)");
+		notHi.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				if (meanCounter == 0) {
+					System.out.println("WOW! Don't be so mean. Please say 'Hi'");
+				}
+				else if (meanCounter == 1) {
+					System.out.println("Wow... Did I do something wrong?");
+				}
+				else if (meanCounter == 2) {
+					System.out.println("You know we can talk things out...right?");
+				}
+				else if (meanCounter == 3) {
+					System.out.println("Whatever... I guess... I was having a bad day but it's ok... I suppose...");
+				}
+				else {
+					System.out.println("...");
+				}
+				HelloCS151.meanCounter++;
+			}
+		});
+		StackPane root = new StackPane();
+		root.getChildren().add(hi);
+		root.getChildren().add(notHi);
+		primaryStage.setScene(new Scene(root, 500, 250));
+		primaryStage.show();
+	}
+}
